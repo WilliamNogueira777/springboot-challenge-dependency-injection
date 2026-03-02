c5bd4daa671ce2efe0a195c65986760b387a24ef
William Nogueira <wyllyam11@hotmail.com>
2026-03-02 17:38:48 -0300

    Criação do projeto: Fase Inicial

---
+++ src/main/java/com/springchallenge/springboot_java_challenge/SpringbootJavaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.springchallenge.springboot_java_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJavaChallengeApplication.class, args);
+	}
+
+}
